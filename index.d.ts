19e4e3aaca22c1a16914c9415ff448c0dc1b85b0
Yosua Ian Sebastian <developer@darcien.dev>
2021-09-08 12:10:41 +0700

    chore: ignore compiled files

--- index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="lodash" />
-export declare function verbose(): void;
-export declare function generateTemplateClassesFromXSD(xsdFilePath: string, dependencies?: Map<string, string>, xmlnsName?: string): void;
